b29cd3fe6e9b9b8f4e10a22bf00c58655a6f8d6b
sindreoestrem <sindrost@stud.ntnu.no>
2019-10-30 23:37:22 +0100

    Getting parameters from url, test

--- pkg/CO2Handlers/CommitsHandler.go
+++ pkg/CO2Handlers/CommitsHandler.go
@@ -1,7 +1,13 @@
 package CO2Handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func CommitsHandler (w http.ResponseWriter, r *http.Request){
-
+	limitParam := r.URL.Query().Get("limit")
+	authParam := r.URL.Query().Get("auth")
+	fmt.Print(limitParam)
+	fmt.Print(authParam)
 }
