b0c05ae4323f736eb9f3a6d7ea98417452be50f3
sindreoestrem <sindrost@stud.ntnu.no>
2019-11-02 17:51:00 +0100

    attempt at paging the results so we get them all

--- pkg/CO2Handlers/CommitsHandler.go
+++ pkg/CO2Handlers/CommitsHandler.go
@@ -6,18 +6,19 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sort"
 	"strconv"
 )
 
 func CommitsHandler (w http.ResponseWriter, r *http.Request){
-	//limitParam := r.URL.Query().Get("limit")
+	limitParam := r.URL.Query().Get("limit")
 	authParam := r.URL.Query().Get("auth")
 	authString := ""
 	if authParam != "" {
 		authString = "private_token=" + authParam
 	}
 	log.Print(authString)
-	response, err := http.Get(API_PATH + "projects?" + authString + "&per_page=100")
+	response, err := http.Get(API_PATH + "projects?" + authString + "&per_page=100&page=1")
 	var project CO2Struct.Project
 	if err != nil {
 		w.WriteHeader(http.StatusFailedDependency)
@@ -30,16 +31,42 @@ func CommitsHandler (w http.ResponseWriter, r *http.Request){
 		if err1 != nil {
 			log.Print(err)
 		}
-		log.Print(project)
-		for i := 0; i < len(project.Repos) ; i++  {
-			commit, _ := http.Get(API_PATH + "projects/" + strconv.FormatInt(project.Repos[i].ID, 10) + "/repository/commits?" + authString)
-			project.Repos[i].Repos, _ = strconv.ParseInt(commit.Header.Get("X-Total"), 10, 64)
+		totalPages, _ := strconv.Atoi(response.Header.Get("X-Total-Pages"))
+
+		for j := 0; j < totalPages ; j++  {
+			var repo CO2Struct.Commits
+			for i := 0 + (j * 100); i < len(project.Repos) ; i++  {
+				commit, _ := http.Get(API_PATH + "projects/" + strconv.FormatInt(project.Repos[i].ID, 10) + "/repository/commits?" + authString)
+				project.Repos[i].Repos, _ = strconv.ParseInt(commit.Header.Get("X-Total"), 10, 64)
+			}
+			if j < (totalPages-1){
+				response, err = http.Get(API_PATH + "projects?" + authString + "&per_page=100&page=" + strconv.Itoa(j+2))
+				body, error := ioutil.ReadAll(response.Body)
+				if error != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+
+				json.Unmarshal(body, &repo)
+			}
+			project.Repos = append(project.Repos, repo)
 		}
+
 		if authString != "" {
 			project.Auth = true
 		} else {
 			project.Auth = false
 		}
+
+		log.Print(len(project.Repos))
+
+		sort.Slice(project.Repos, func(i, j int) bool {
+			return project.Repos[i].Repos > project.Repos[j].Repos
+		})
+		limit, _ := strconv.ParseInt(limitParam, 10, 64)
+		log.Print(limitParam)
+		log.Print(limit)
+		project.Repos = project.Repos[:limit]
+
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(project)
