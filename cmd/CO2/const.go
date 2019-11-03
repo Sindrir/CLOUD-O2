8faa5003459bddda43a4e038b3ae3d742f4d2e87
sindreoestrem <sindrost@stud.ntnu.no>
2019-11-03 23:17:50 +0100

    const location change

--- cmd/CO2/const.go
+++
@@ -1,6 +0,0 @@
-package main
-
-const COMMITS_PATH		= "/repocheck/v1/commits"
-const LANGUAGES_PATH	= "/repocheck/v1/languages"
-const ISSUES_PATH		= "/repocheck/v1/issues"
-const STATUS_PATH		= "/repocheck/v1/status"
--- cmd/CO2/main.go
+++ cmd/CO2/main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+const COMMITS_PATH		= "/repocheck/v1/commits"
+const LANGUAGES_PATH	= "/repocheck/v1/languages"
+const ISSUES_PATH		= "/repocheck/v1/issues"
+const STATUS_PATH		= "/repocheck/v1/status"
+
 func main () {
 
 	port := os.Getenv("PORT")
